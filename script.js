06c9a93f0839b5b66d655eafc8419c09d4e1b24f
shinysu <shiny.suresh@gmail.com>
2021-04-28 15:40:51 +0530

    resized canvas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let animals = ['deer', 'elephant', 'frog', 'kangaroo', 'lion', 'zebra']
 
 function display_animal(){
-    animalName = document.getElementById("msg_input").value.toLowerCase();
+    animalName = document.getElementById("msg_input").value.toLowerCase().trim();
     document.getElementById("msg_input").value = "";
     if (animals.includes(animalName)){
         displayImage(animalName)
@@ -23,14 +23,16 @@ function displayImage(animalName){
 function fillImageInCanvas(image){
     let canvas = document.getElementById("myCanvas");
     let context = canvas.getContext("2d");
+    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener('orientationchange', resizeCanvas, false);
+    resizeCanvas(canvas)
     context.fillStyle = '#f8f8f8';
     context.fillRect(0, 0, canvas.width, canvas.height);
-    var hRatio = canvas.width / image.width    ;
-    var vRatio = canvas.height / image.height  ;
-    var ratio  = Math.min ( hRatio, vRatio );
-    var centerShiftX = ( canvas.width - image.width*ratio ) / 2;
-    var centerShiftY = ( canvas.height - image.height*ratio ) / 2;  
-    context.drawImage(image, 0, 0, image.width, image.height, centerShiftX, centerShiftY, image.width*ratio, image.height*ratio);
+    var hratio = canvas.width / image.width    ;
+    var vratio = canvas.height / image.height  ;
+    var centerShiftX = ( canvas.width - image.width*hratio ) / 2;
+    var centerShiftY = ( canvas.height - image.height*vratio ) / 2; 
+    context.drawImage(image, 0, 0, image.width, image.height, centerShiftX, centerShiftY, image.width*hratio, image.height*vratio);
 }
 
 function displayNotFound(animalName){
@@ -42,3 +44,8 @@ function displayNotFound(animalName){
     text = animalName.charAt(0).toUpperCase() + animalName.substring(1, animalName.length) + " is not found"
     context.fillText(text, 10, 50);
 }
+
+function resizeCanvas(canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
