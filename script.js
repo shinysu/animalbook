bbc128d21cedb6da4646a1c32e30e146134003ce
shinysu <shiny.suresh@gmail.com>
2021-04-30 14:37:35 +0530

    included voice

--- script.js
+++ script.js
@@ -11,31 +11,35 @@ input.addEventListener("keyup", function(event) {
 function display_animal(){
     animalName = document.getElementById("msg_input").value.toLowerCase().trim();
     document.getElementById("msg_input").value = "";
-    if (animals.includes(animalName)){
-        displayImage(animalName)
-    }
-    else{
-        displayNotFound(animalName)
-    }
+    displayImage(animalName);
 }
 
 function displayImage(animalName){
-    let image = new Image();
-    image.src = "images/"+animalName+".jpg";
-    image.onload = function(){
-        fillImageInCanvas(image)
-    }
-    console.log(animalName);
-}
-
-function fillImageInCanvas(image){
     let canvas = document.getElementById("myCanvas");
     let context = canvas.getContext("2d");
+    if (animals.includes(animalName)){
+        let image = new Image();
+        image.src = "images/"+animalName+".jpg";
+        image.onload = function(){
+            setUpCanvas(canvas, context);
+            fillImageInCanvas(canvas, context, image);
+        }
+    }
+    else{
+        setUpCanvas(canvas, context);
+        imageNotFound(canvas, context, animalName);
+    }
+}
+
+function setUpCanvas(canvas, context){
     window.addEventListener('resize', resizeCanvas, false);
     window.addEventListener('orientationchange', resizeCanvas, false);
-    resizeCanvas(canvas)
+    resizeCanvas(canvas);
     context.fillStyle = '#f8f8f8';
     context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function fillImageInCanvas(canvas, context, image){
     var hratio = canvas.width / image.width    ;
     var vratio = canvas.height / image.height  ;
     var centerShiftX = ( canvas.width - image.width*hratio ) / 2;
@@ -43,13 +47,7 @@ function fillImageInCanvas(image){
     context.drawImage(image, 0, 0, image.width, image.height, centerShiftX, centerShiftY, image.width*hratio, image.height*vratio);
 }
 
-function displayNotFound(animalName){
-    let canvas = document.getElementById("myCanvas");
-    let context = canvas.getContext("2d");
-    window.addEventListener('resize', resizeCanvas, false);
-    window.addEventListener('orientationchange', resizeCanvas, false);
-    resizeCanvas(canvas)
-    context.clearRect(0, 0, canvas.width, canvas.height);
+function imageNotFound(canvas, context, animalName){
     context.font = "40px Arial";
     context.fillStyle = "#337ab7";
     text = animalName.charAt(0).toUpperCase() + animalName.substring(1, animalName.length) + " is not found"
@@ -59,24 +57,18 @@ function displayNotFound(animalName){
 function resizeCanvas(canvas) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-  }
+}
 
-  function runSpeechRecognition() {
-   // var output = document.getElementById("output");
-    //var action = document.getElementById("action");
-    // new speech recognition object
-    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+function runSpeechRecognition() {
+    var SpeechRecognition = window.webkitSpeechRecognition;
     var recognition = new SpeechRecognition();
 
-    // This runs when the speech recognition service starts
-    recognition.onstart = function() {
+    recognition.onstart = function() {     
         document.getElementById('btn_voice').style.backgroundColor = '#67c067';
         document.getElementById("msg_input").value = "listening...";
-     //   action.innerHTML = "<small>listening, please speak...</small>";
     };
     
     recognition.onspeechend = function() {
-    //    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
         recognition.stop();
         document.getElementById('btn_voice').style.backgroundColor = '#ffffff';
         
@@ -88,16 +80,8 @@ function resizeCanvas(canvas) {
         setTimeout(function(){
             document.getElementById("msg_input").value = '';
         }, 3000);
-       // var confidence = event.results[0][0].confidence;
-        console.log(animalName);
-     //   output.classList.remove("hide");
-     if (animals.includes(animalName)){
-        displayImage(animalName)
-    }
-    else{
-        displayNotFound(animalName)
-    }
+        displayImage(animalName);
     };
-  
+
     recognition.start();
 }
